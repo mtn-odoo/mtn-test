808f7f1228ace7dd7bbe388ebe00bf7b949c5768
Matthieu Tans (mtn) <mtn@odoo.com>
2023-01-30 11:37:44 +0100

    go mtn

---
+++ Estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name' : 'Estate',
+    'version' : '1.2',
+    'summary': 'estate mtn',
+    'sequence': 10,
+    'description': """
+test test
+    """,
+    'category': 'mtn test',
+    'depends' : ['base_setup'],
+    'data': [
+        'views/estate.xml',
+    ],
+    'demo': [
+        'demo/account_demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
