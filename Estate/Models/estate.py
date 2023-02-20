202cb56bd1c9d1bb0048763fc3f3af4e952d276d
Matthieu Tans (mtn) <mtn@odoo.com>
2023-02-20 10:29:23 +0100

    push champs

--- Estate/Models/estate.py
+++ Estate/Models/estate.py
@@ -5,3 +5,19 @@ class estate(models.Model):
     _description = "pour vendre ta dar"
 
     name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_avaibility = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Orientation',
+        selection=[('nord','Nord'),('sud','Sud'),('est','Est'),('ouest','Ouest')]
+    )
+    
