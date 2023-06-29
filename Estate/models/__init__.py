c7836e835b6cf7fe11b0dc82de1ded9a3d01154e
Matthieu Tans (mtn) <mtn@odoo.com>
2023-06-29 14:14:17 +0200

    error

--- Estate/models/__init__.py
+++ Estate/models/__init__.py
@@ -1,2 +1,2 @@
 from . import estate
-from . import estate.property
+from . import estate_property
--- Estate/models/estate.property.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class estate(models.Model):
-    _name = "estate.property"
-    _description = "type de bien"
-
-    name = fields.Char(required=True, index=True)
-
-    
